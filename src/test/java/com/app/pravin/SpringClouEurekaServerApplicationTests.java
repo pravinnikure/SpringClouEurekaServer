d979f9c0dc89b8e55994a002370ac31bf106b486
pravinjenkins <pravinnikure300@gmail.com>
2024-02-21 18:06:43 +0530

    First Commit

---
+++ src/test/java/com/app/pravin/SpringClouEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.pravin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringClouEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
